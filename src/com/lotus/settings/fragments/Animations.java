Havoc-OS/android_packages_apps_HavocSettings

Power Menu animations [2/2]

src/com/havoc/settings/fragments/Animations.java
@@ -71,6 +71,7 @@ public class Animations extends SettingsPreferenceFragment  implements Preferenc
     private static final String PREF_TILE_ANIM_STYLE = "qs_tile_animation_style";
     private static final String PREF_TILE_ANIM_DURATION = "qs_tile_animation_duration";
     private static final String PREF_TILE_ANIM_INTERPOLATOR = "qs_tile_animation_interpolator";
+    private static final String POWER_MENU_ANIMATIONS = "power_menu_animations";
 
     private ListPreference mScreenOffAnimation; 
     private ListPreference mToastAnimation;
@@ -92,6 +93,7 @@ public class Animations extends SettingsPreferenceFragment  implements Preferenc
     ListPreference mWallpaperClose;
     ListPreference mWallpaperIntraOpen;
     ListPreference mWallpaperIntraClose;
+    private ListPreference mPowerMenuAnimations;
 
     private int[] mAnimations;
     private String[] mAnimationsStrings;
@@ -252,6 +254,12 @@ public class Animations extends SettingsPreferenceFragment  implements Preferenc
         mWallpaperIntraClose.setEntryValues(mAnimationsNum);
         mWallpaperIntraClose.setOnPreferenceChangeListener(this);
 
+        mPowerMenuAnimations = (ListPreference) findPreference(POWER_MENU_ANIMATIONS);
+        mPowerMenuAnimations.setValue(String.valueOf(Settings.System.getInt(
+                getContentResolver(), Settings.System.POWER_MENU_ANIMATIONS, 0)));
+        mPowerMenuAnimations.setSummary(mPowerMenuAnimations.getEntry());
+        mPowerMenuAnimations.setOnPreferenceChangeListener(this);
+
         if (mToast != null) {
             mToast.cancel();
             mToast = null;
@@ -391,6 +399,12 @@ public class Animations extends SettingsPreferenceFragment  implements Preferenc
             Settings.System.putInt(resolver,
                     Settings.System.ACTIVITY_ANIMATION_CONTROLS[10], val);
             preference.setSummary(getProperSummary(preference));
+            return true;
+        } else if (preference == mPowerMenuAnimations) {
+            Settings.System.putInt(getContentResolver(), Settings.System.POWER_MENU_ANIMATIONS,
+                    Integer.valueOf((String) newValue));
+            mPowerMenuAnimations.setValue(String.valueOf(newValue));
+            mPowerMenuAnimations.setSummary(mPowerMenuAnimations.getEntry());
             return true;
         }
         return false;

hungphan2001/android_packages_apps_Lotus

src/com/lotus/settings/fragments/Animations.java
@@ -84,6 +84,7 @@ public class Animations extends SettingsPreferenceFragment implements OnPreferen
     private static final String SCROLLINGCACHE_PERSIST_PROP = "persist.sys.scrollingcache";
 
     private static final String SCROLLINGCACHE_DEFAULT = "2";
+    private static final String POWER_MENU_ANIMATIONS = "power_menu_animations";
 
     ListPreference mActivityOpenPref;
     ListPreference mActivityClosePref;
@@ -108,6 +109,7 @@ public class Animations extends SettingsPreferenceFragment implements OnPreferen
 
     protected Context mContext;
     protected ContentResolver mContentRes;
+    private ListPreference mPowerMenuAnimations;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -245,6 +247,12 @@ public class Animations extends SettingsPreferenceFragment implements OnPreferen
         mScrollingCachePref.setValue(SystemProperties.get(SCROLLINGCACHE_PERSIST_PROP,
                 SystemProperties.get(SCROLLINGCACHE_PERSIST_PROP, SCROLLINGCACHE_DEFAULT)));
         mScrollingCachePref.setOnPreferenceChangeListener(this);
+
+        mPowerMenuAnimations = (ListPreference) findPreference(POWER_MENU_ANIMATIONS);
+        mPowerMenuAnimations.setValue(String.valueOf(Settings.System.getInt(
+                getContentResolver(), Settings.System.POWER_MENU_ANIMATIONS, 0)));
+        mPowerMenuAnimations.setSummary(mPowerMenuAnimations.getEntry());
+        mPowerMenuAnimations.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -334,6 +342,12 @@ public class Animations extends SettingsPreferenceFragment implements OnPreferen
             if (objValue != null) {
                 SystemProperties.set(SCROLLINGCACHE_PERSIST_PROP, (String) objValue);
             }
+            return true;
+        } else if (preference == mPowerMenuAnimations) {
+            Settings.System.putInt(getContentResolver(), Settings.System.POWER_MENU_ANIMATIONS,
+                    Integer.valueOf((String) objValue));
+            mPowerMenuAnimations.setValue(String.valueOf(objValue));
+            mPowerMenuAnimations.setSummary(mPowerMenuAnimations.getEntry());
             return true;
         }
 	 preference.setSummary(getProperSummary(preference));